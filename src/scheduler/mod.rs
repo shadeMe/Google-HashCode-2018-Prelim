ad637948687f3dc8a09875371eaa3a70a973a130
shadeMe <shademehere@gmail.com>
2018-03-06 21:00:04 +0100

    Migrate to CLion/Rust More code Run into a trait object wall

--- src/scheduler/mod.rs
+++ src/scheduler/mod.rs
@@ -1,2 +1,205 @@
-pub mod job_scheduler;
-pub mod job;
+use util::{Coord, TimeStep, FileReader, Counter};
+use vehicle::Vehicle;
+use std::collections::{HashMap, BinaryHeap, HashSet};
+use std::rc::Rc;
+use std::cmp::Ordering;
+use std::io::Error;
+use std::hash::Hash;
+
+pub type JobId = i32;
+
+pub trait HasJob: Ord {
+	fn id(&self) -> JobId;
+	fn start(&self) -> Coord;
+	fn end(&self) -> Coord;
+	fn earliest_start(&self) -> TimeStep;
+	fn latest_finish(&self) -> TimeStep;
+}
+
+#[derive(Eq, Hash)]
+pub struct Job {
+	id:				JobId,
+	start:			Coord,
+	end:			Coord,
+	earliest_start:	TimeStep,
+	latest_end:		TimeStep,
+}
+
+impl Job {
+	fn cmp_ascending(&self, other: &Job) -> Ordering {
+		if self.earliest_start < other.earliest_start {
+			Ordering::Less
+		} else if self.earliest_start > other.earliest_start {
+			Ordering::Greater
+		} else {
+			// TODO compare start and end coords?
+			Ordering::Equal
+		}
+	}
+}
+
+/// Sorted by the earliest start first
+impl PartialOrd for Job {
+	fn partial_cmp(&self, other: &Job) -> Option<Ordering> {
+		Some(other.cmp_ascending(&self))
+	}
+}
+impl PartialEq for Job {
+	fn eq(&self, other: &Job) -> bool {
+		other.cmp_ascending(&self) == Ordering::Equal
+	}
+}
+impl Ord for Job {
+	fn cmp(&self, other: &Job) -> Ordering {
+		// We flip the ordering as we need a min-heap
+		other.cmp_ascending(&self)
+	}
+}
+
+impl HasJob for Job {
+	fn id(&self) -> JobId {
+		self.id
+	}
+	fn start(&self) -> Coord {
+		self.start.clone()
+	}
+	fn end(&self) -> Coord {
+		self.end.clone()
+	}
+	fn earliest_start(&self) -> TimeStep {
+		self.earliest_start
+	}
+	fn latest_finish(&self) -> TimeStep {
+		self.latest_end
+	}
+}
+
+pub type JobPtr = Rc<HasJob>;
+type VehPtr = Rc<Vehicle>;
+
+/// Context for a single simulation timestep 
+pub struct NewTick {
+	pub current_step:	TimeStep,
+}
+
+/// Context for a new job
+pub struct NewJob {
+	pub job:			JobPtr,
+	pub current_step:	TimeStep,
+}
+
+/// Output of a single simulation timestep
+pub enum TickComplete {
+	/// Continue execution
+	Continue,
+	/// Update current job
+	Reschedule(JobPtr),
+	/// Assign new job
+	JobComplete(Coord),
+}
+
+struct JobScheduler {
+	num_rows:			i32,
+	num_cols:			i32,
+	num_vehicles:		i32,
+	num_rides:			i32,
+	ride_bonus:			i32,
+	max_tsteps:			TimeStep,
+
+	current_step:		TimeStep,
+	fleet:              Vec<VehPtr>,
+	jobs:               Vec<JobPtr>,
+
+	grid:               HashMap<Coord, HashSet<VehPtr>>,
+	rem_jobs:           BinaryHeap<JobPtr>,
+}
+
+impl JobScheduler {
+	fn new(input: &mut FileReader) -> JobScheduler {
+		let mut out = JobScheduler {
+			num_rows: 0,
+			num_cols: 0,
+			num_vehicles: 0,
+			num_rides: 0,
+			ride_bonus: 0,
+			max_tsteps: 0,
+			current_step: 0,
+			fleet: Vec::default(),
+			jobs: Vec::default(),
+			grid: HashMap::default(),
+			rem_jobs: BinaryHeap::default(),
+		};
+
+		let reader = |r : Result<(&str, &Counter), Error>| {
+			match r {
+				Ok((l, c)) => {
+					let mut splits: Vec<i32> = l.split(' ').map(|s| s.parse::<i32>().unwrap()).collect();
+					assert_eq!(splits.len(), 6);
+
+					match c.is_start() {
+						true => {
+							out.num_rows = splits[0];
+							out.num_cols = splits[1];
+							out.num_vehicles = splits[2];
+							out.num_rides = splits[3];
+							out.ride_bonus = splits[4];
+							out.max_tsteps = splits[5];
+
+							out.current_step = 0;
+							out.fleet = Vec::with_capacity(out.num_vehicles as usize);
+							out.jobs = Vec::with_capacity(out.num_rides as usize);
+							out.grid = HashMap::with_capacity((out.num_rows * out.num_cols) as usize);
+							out.rem_jobs = BinaryHeap::with_capacity(out.num_rides as usize);
+						},
+						_ => {
+							let x_start = splits[0];
+							let y_start = splits[1];
+							let x_end = splits[2];
+							let y_end = splits[3];
+							let early = splits[4];
+							let late = splits[5];
+
+							let jerb = Job{
+								id: c.cur() - 1,       // the jobs start from the second line in the input file
+								start: Coord::new(x_start, y_start),
+								end: Coord::new(x_end, y_end),
+								earliest_start: early,
+								latest_end: late,
+							};
+
+							out.jobs.push(Rc::new(jerb));
+						}
+					};
+				},
+				Err(e) => print!("Error reading input. Error: {:?}", e),
+			};
+		};
+
+		// parse input
+		input.read_all_lines(reader);
+
+		// init fleet and grid
+		for i in 0..out.num_vehicles {
+			out.fleet.push(Rc::new(Vehicle::new(i)));
+		}
+
+		for x in 0..out.num_rows {
+			for y in 0..out.num_cols {
+				let mut set = HashSet::<VehPtr>::new();
+				let coord = Coord::new(x, y);
+
+				// all vehicles start at the origin
+				if coord.is_origin() {
+					for v in out.fleet {
+						set.insert(v.clone());
+					}
+				}
+				out.grid.insert(coord, set);
+			}
+		}
+
+		// TODO sort the jobs into the heap
+
+		out
+	}
+}
--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,22 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std;
+
 fn manhattan_dist(a: &Coord, b: &Coord) -> i32 {
 	i32::abs(a.x - b.x) + i32::abs(a.y - b.y)
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Eq, Hash)]
 pub struct Coord {
 	pub x:	i32,
 	pub y:	i32
 }
 
 impl Coord {
+	pub fn new(x: i32, y: i32) -> Coord {
+		Coord{x, y}
+	}
+
 	pub fn dist(&self, a: &Coord) -> i32 {
 		manhattan_dist(self, a)
 	}
@@ -31,26 +39,56 @@ impl PartialEq for Coord {
 }
 
 pub struct Counter {
-	c:	i32,
+	counter:	i32,
+	start:		i32,
 }
 
 impl Counter {
 	pub fn start_zero() -> Counter {
-		Counter{c: 0}
+		Counter{counter: 0, start: 0}
 	} 	
 	
 	pub fn start_one() -> Counter {
-		Counter{c: 1}
+		Counter{counter: 1, start: 1}
 	}
 	
 	pub fn cur(&self) -> i32 {
-		self.c
+		self.counter
 	}
 	
 	pub fn inc(&mut self) -> () {
-		self.c += 1
-	} 
+		self.counter += 1
+	}
+
+	pub fn is_start(&self) -> bool {
+		self.counter == self.start
+	}
 }
 
 
 pub type TimeStep = i32;
+
+pub struct FileReader {
+	line_counter:	Counter,
+	reader:			BufReader<File>,
+}
+
+impl FileReader {
+	pub fn new(path: &str) -> Result<FileReader, std::io::Error> {
+		let mut out = FileReader{
+			line_counter: Counter::start_zero(),
+			reader: BufReader::new(try!(File::open(path))),
+		};
+
+		Ok(out)
+	}
+
+	pub fn read_all_lines<F>(&mut self, delegate: F)
+		where F: FnOnce(Result<(&str, &Counter), std::io::Error>)
+	{
+		self.reader.lines().for_each(|l| {
+			delegate(l.map(|s| (s.as_str(), &self.line_counter)));
+			self.line_counter.inc();
+		});
+	}
+}
